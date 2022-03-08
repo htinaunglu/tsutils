cd97f5fc16f1040f95b091ea650ceec16650d665
Htin Aung Lu <htinaunglu@outlook.com>
2022-03-08 10:16:19 +0630

    0.0.9 update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'tsautils',         # How you named your package folder (MyLib)
   packages = ['tsautils'],   # Chose the same as "name"
-  version = '0.0.7',      # Start with a small number and increase it with every change you make
+  version = '0.0.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Time Series Utilities',   # Give a short description about your library
-  author = 'htinaunglu',                   # Type in your name
-  author_email = 'htinaunglu@outlook.com',      # Type in your E-Mail
-  url = 'https://github.com/htinaunglu/tsutils/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/htinaunglu/tsutils.git',    # I explain this later on
+  author = 'Anonymous_MM',                   # Type in your name
+  author_email = '9r4yh4ck3r@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/closed',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/closed.git',    # I explain this later on
   keywords = ['Utilities', 'LSTM', 'Time Series'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
